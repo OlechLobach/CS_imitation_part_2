edc0224119a683312a92859c40f5e8a9d74eeeb1
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-17 13:45:11 +0200

    Add project files.

---
+++ CS_imitation_part_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_imitation_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
